bd53cd5a1dc2fc048b6545dcdf049cbb30118c29
Alex <33266159+Bambarello@users.noreply.github.com>
2018-11-15 17:35:29 +0200

    Crontab job update

--- mnchecker-install.sh
+++ mnchecker-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo git clone https://github.com/Bambarello/mnchecker /root/mnchecker
 crontab -l > tempcron
-echo "*/30 * * * * /root/mnchecker/mnchecker --currency-handle=\"/root/binarium/binarium\" --currency-bin-cli=\"/root/binarium/binarium-cli\" --currency-datadir=\"/root/.binariumcore\" > /root/mnchecker/mnchecker-cron.log 2>&1"  >> tempcron
+echo "*/30 * * * * /root/mnchecker/mnchecker --currency-handle=\"binarium\" --currency-bin-cli=\"/root/binarium/binarium-cli\" --currency-bin-daemon=\"/root/binarium/binariumd" --currency-datadir=\"/root/.binariumcore\" > /root/mnchecker/mnchecker-cron.log 2>&1"  >> tempcron
 crontab tempcron
 rm tempcron
