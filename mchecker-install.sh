f2b5688e09ece045bf0be835fb92e4c3c110022d
Alex <33266159+Bambarello@users.noreply.github.com>
2018-11-15 17:18:25 +0200

    Add files via upload

---
+++ mchecker-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo git clone https://github.com/Bambarello/mnchecker /root/mnchecker
+crontab -l > tempcron
+echo "*/30 * * * * /root/mnchecker/mnchecker --currency-handle=\"binarium\" --currency-bin-cli=\"binarium-cli\" --currency-datadir=\"/root/.binariumcore\" > /root/mnchecker/mnchecker-cron.log 2>&1" >> tempcron
+crontab tempcron
+rm tempcron
