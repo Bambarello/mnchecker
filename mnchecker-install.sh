b7cf146c74e139ed8ab6fbd0a7d4f08420cec4fc
Alex <33266159+Bambarello@users.noreply.github.com>
2018-11-18 20:31:38 +0200

    Modified for Binarium

--- mnchecker-install.sh
+++ mnchecker-install.sh
@@ -4,3 +4,4 @@ crontab -l > tempcron
 echo "*/30 * * * * /root/mnchecker/mnchecker --currency-handle=\"binarium\" --currency-bin-cli=\"/root/binarium/binarium-cli\" --currency-bin-daemon=\"/root/binarium/binariumd\" --currency-datadir=\"/root/.binariumcore\" > /root/mnchecker/mnchecker-cron.log 2>&1"  >> tempcron
 crontab tempcron
 rm tempcron
+echo "mnchecker and crontab installed, for duplicates checking run crontab -e"
